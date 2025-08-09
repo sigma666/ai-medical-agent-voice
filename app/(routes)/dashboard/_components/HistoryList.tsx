8896af9647719d559bf261739ebf4eecad1f836a
Lumiere Moses <108468055+LinuxCode96@users.noreply.github.com>
2025-08-09 21:57:01 +0200

    second commit at 9h:Am 09-august-2025

--- app/(routes)/dashboard/_components/HistoryList.tsx
+++ app/(routes)/dashboard/_components/HistoryList.tsx
@@ -7,8 +7,16 @@ function HistoryList() {
   return (
     <div>
       {historylist.length === 0 ? (
-        <div>
-          <Image src={"/"} alt={"logo"} width={190} height={50} />
+        <div className="flex items-center justify-center flex-col p-7 border border-dashed rounded-2xl">
+          <Image
+            src={"/agent-med.svg"}
+            alt={"logo"}
+            width={190}
+            height={50}
+            className=""
+          />
+          <h2 className="font-bold text-xl">No Recent Consultations</h2>
+          <p>It looks like you haven't consulted with any doctor yet</p>
         </div>
       ) : (
         <div>
--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -111,7 +111,7 @@ const Navbar = () => {
     <nav className="flex w-full items-center justify-between border-t border-b border-neutral-200 px-4 py-4 dark:border-neutral-800">
       <div className="flex items-center gap-2">
         <div className="size-7 rounded-full bg-gradient-to-br from-violet-500 to-pink-500" />
-        <h1 className="text-base font-bold md:text-2xl">Aceternity UI</h1>
+        <h1 className="text-base font-bold md:text-2xl">AI MEDICAL VOICE</h1>
       </div>
       {!user ? (
         <Link href={"/sign-in"}>
